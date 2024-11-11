a9624a89464835a377e9a3eb9c2fc793d2be30da
Eldor <82472210+BaKozak@users.noreply.github.com>
2024-11-11 19:37:20 +0100

    create stop watch component test file

---
+++ src/StopWatch/StopWatch.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import StopWatch from './StopWatch'
+
+describe('<StopWatch />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<StopWatch />)
+  })
+})
