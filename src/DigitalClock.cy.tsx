6e067781f313afd2840b533aafb1aa75b893b0a3
Eldor <82472210+BaKozak@users.noreply.github.com>
2024-11-11 19:34:15 +0100

    mouting existing components check

---
+++ src/DigitalClock.cy.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import DigitalClock from './DigitalClock'
+
+describe('<DigitalClock />', () => {
+  it('renders', () => {
+    // see: https://on.cypress.io/mounting-react
+    cy.mount(<DigitalClock />)
+  })
+})
