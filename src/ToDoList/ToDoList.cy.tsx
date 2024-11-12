e2a17f3866983dea433fe358a513ac9176aabb21
Eldor <82472210+BaKozak@users.noreply.github.com>
2024-11-12 17:52:43 +0100

    partialy add todo test

--- src/ToDoList/ToDoList.cy.tsx
+++ src/ToDoList/ToDoList.cy.tsx
@@ -1,19 +1,22 @@
 import ToDoList from './ToDoList'
 
 describe('Todo List component', () => {
+  const listOfDefaultItems = [
+    'Wake up', 'Take a shower', 'Eat breakfast', 'Go to daily'
+  ];
+
+  beforeEach('mount component', () => {
+    cy.mount(<ToDoList />)
+  });
 
   context('Default state tests', () => {
     it('should check if header of component is correct', () => {
-      cy.mount(<ToDoList />)
-
       cy.get('[data-cy="todo-title"]')
         .should('be.visible')
         .and('have.text', 'My TODO List');
     });
 
     it('should check if input section of component is correct', () => {
-      cy.mount(<ToDoList />)
-
       cy.get('[data-cy="task-input-section"]')
         .find('input')
         .should('be.visible')
@@ -26,13 +29,8 @@ describe('Todo List component', () => {
     });
 
     it('should check if default state of List is correct', () => {
-      const listOfDefaultItems = [
-        'Wake up', 'Take a shower', 'Eat breakfast', 'Go to daily'
-      ];
+      cy.get('[data-cy="todo-task-list"]').children().should('have.length', listOfDefaultItems.length);
 
-      cy.mount(<ToDoList />)
-
-      cy.get('[data-cy="todo-task-list"]').children().should('have.length', 4);
       cy.get('[data-cy="todo-task-list"]').children().each((item, index) => {
         cy.wrap(item).within(() => {
           cy.get('span').should('include.text', listOfDefaultItems[index]);
@@ -42,6 +40,42 @@ describe('Todo List component', () => {
           cy.get('button').eq(2).should('have.text', 'Down');
         });
       });
+    });
+  });
+
+  context.only('State manipulation tests', () => {
+    const newTaskName = "Write cypress tests"
+
+    it('should be able to input text in input field and store as value', () => {
+      cy.get('[data-cy="task-input-section"] input').type(newTaskName);
+      cy.get('[data-cy="task-input-section"] input').should('have.value', newTaskName);
+    });
+
+    it('should be able to add a todo task using add button', () => {
+      cy.get('[data-cy="task-input-section"] input').type(newTaskName);
+      cy.get('[data-cy="task-input-section"] button').click();
+
+      cy.get('[data-cy="todo-task-list"]').children().should('have.length', listOfDefaultItems.length + 1);
+      cy.get('[data-cy="todo-task-list"]').children().last().within(() => {
+        cy.get('span').should('include.text', newTaskName);
+        cy.get('button').should('have.length', 3);
+        cy.get('button').eq(0).should('have.text', 'Delete');
+        cy.get('button').eq(1).should('have.text', 'Up');
+        cy.get('button').eq(2).should('have.text', 'Down');
+      });
+
+      it('should be able to remove a todo task using the delete button', () => {
+
+      });
+
+      it('should be able to move up a todo task using the up button', () => {
+
+      });
+
+      it('should be able to move down a todo task using the down button', () => {
+
+      });
+
     });
   });
 });
